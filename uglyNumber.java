f716d093596ac58aae46d7cf73ba3792ab77a7f2
sudigh <sudiptagh@yahoo.com>
2016-07-25 22:47:05 -0700

    Update uglyNumber.java

--- uglyNumber.java
+++ uglyNumber.java
@@ -1,4 +1,7 @@
 class Solution {
+  /* Ugly numbers are positive numbers whose prime factors only include 2, 3, 5. For example, 6, 8 are ugly while 14 is not ugly since it includes another prime factor 7.
+
+Note that 1 is typically treated as an ugly number. */
   public static void main(String[] args) {
    
     System.out.println("11 : " + isUgly(11));
