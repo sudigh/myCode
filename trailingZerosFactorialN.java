4c460e7d9eca33d9de90da2f42b1b48b47d3e2c5
sudigh <sudiptagh@yahoo.com>
2016-07-26 08:19:56 -0700

    Update trailingZerosFactorialN.java

--- trailingZerosFactorialN.java
+++ trailingZerosFactorialN.java
@@ -1,4 +1,8 @@
 public class Solution {
+/* Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.*/
+// REF : http://www.purplemath.com/modules/factzero.htm
     public int trailingZeroes(int n) {
         int count = 0;
         while (n > 4) {
