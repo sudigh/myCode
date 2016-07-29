34635e069a129c13b0df5eb07fb92f6b11ffa6a8
sudigh <sudiptagh@yahoo.com>
2016-07-28 19:42:26 -0700

    Count Numbers with Unique Digits x such that are 0 <= x <= 10^n

--- countNumsWithUniqueDigits.java
+++ countNumsWithUniqueDigits.java
@@ -1,9 +1,36 @@
+/* Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10^n.
+
+Example:
+Given n = 2, return 91. (The answer should be the total numbers in the range of 0 ≤ x < 100, excluding [11,22,33,44,55,66,77,88,99]) */
+/* f(n) = count of numbers with unique digits which are less than 10^n.
+f(1) = [0, 1,2,...,9] = 10
+f(2) = 9 * 9. Because for each number i from 1, ..., 9, we can pick j to form a 2-digit number ij and there are 9 numbers 
+that are different from i for j to choose from.
+
+f(3) = f(2) * 8 = 9 * 9 * 8. Because for each number with unique digits of length 2, say ij, we can pick k to form a 3 digit 
+number ijk and there are 8 numbers that are different from i and j for k to choose from.
+
+Similarly f(4) = f(3) * 7 = 9 * 9 * 8 * 7....
+
+...
+
+f(10) = 9 * 9 * 8 * 7 * 6 * ... * 1
+
+f(11) = 0 = f(12) = f(13)....
+
+any number with length > 10 couldn't be unique digits number.
+
+The problem is asking for numbers from 0 to 10^n. Hence return f(1) + f(2) + .. + f(n)
+
+There are only 11 different ans. You can create a lookup table for it. This problem is O(1) in essence.
+*/
 public class Solution {
     public int countNumbersWithUniqueDigits(int n) {
         if (n == 0) {
             return 1;
         }
-        int totalNums = 10, base = 9, i = 9;
+        int totalNums = 10; // f(1) = 10
+        int base = 9, i = 9;
         while (i > 0 && n > 1) {
             base = base * i;
             totalNums += base;
